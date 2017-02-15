1a2395f05c77511ed804b336271925e9e862892c
aj1155 <aj1155@naver.com>
2017-02-15 18:12:26 +0900

    [api] deploy문제로 yml 설정 변경

--- src/main/java/kr/ac/skhu/ApiServerApplication.java
+++ src/main/java/kr/ac/skhu/ApiServerApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@PropertySource({ "classpath:DB.yml" })
+//@PropertySource({ "classpath:DB.yml" })
 public class ApiServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
