f395256af35b61b77580be776b87675fc121cf55
aj1155 <aj1155@naver.com>
2017-03-06 13:28:55 +0900

    [api] UserRequest not null 제한 조건설정

--- src/main/java/kr/ac/skhu/controller/model/request/UserRequest.java
+++ src/main/java/kr/ac/skhu/controller/model/request/UserRequest.java
@@ -13,21 +13,15 @@ public class UserRequest {
     private int id;
     @NotNull
     private String login_id;
-    @NotNull
     private String password;
     @NotNull
     private String user_name;
     @NotNull
     private String categoryId;
-    @NotNull
     private String email;
-    @NotNull
     private String birth;
-    @NotNull
     private String company_number;
-    @NotNull
     private String phone_number;
-    @NotNull
     private String status;
     private int grade;
     private int user_type;
